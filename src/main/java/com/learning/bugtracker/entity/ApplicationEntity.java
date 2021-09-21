1fa076c8374a00cdb1e437c443c257bb5014a3e6
Francisco Briceño <fjavier.cgam@gmail.com>
2021-09-20 21:13:21 -0600

    Se crea el ApplicationEntity
    Esta entidad nos servira para poder registrar las aplicaciones

---
+++ src/main/java/com/learning/bugtracker/entity/ApplicationEntity.java
@@ -0,0 +1,70 @@
+package java.com.learning.bugtracker.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "application")
+public class ApplicationEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String appName;
+
+    @Column(length = 2000)
+    private String owner;
+
+    private String description;
+
+    public ApplicationEntity(String appName, String owner, String description) {
+        this.appName = appName;
+        this.owner = owner;
+        this.description = description;
+    }
+
+    public ApplicationEntity(){}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Application{" +
+                "id=" + id +
+                ", name='" + appName + '\'' +
+                ", owner=" + owner +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
